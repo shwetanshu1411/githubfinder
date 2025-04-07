7d4a4c6f7f4bf6196aa96134de35251cc655a6ee
shwetanshu1411 <2228149@kiit.ac.in>
2025-04-08 01:26:57 +0530

    update axios file

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,24 @@
+// import axios from "axios";
+
+// const instance = axios.create({
+//   baseURL: "https://api.github.com",
+// });
+
+// export default instance;
+
+
+// axios.ts
 import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://api.github.com",
+  headers: {
+    'Accept': 'application/vnd.github.v3+json',
+    'User-Agent': 'GitHub-User-Finder-App', 
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+
+    
+  },
 });
 
 export default instance;
